cc001481e436e10ec5cd3e66dad56dfefe0bff96
matsutomu <matsu.tom@gmail.com>
2017-08-13 14:45:57 +0900

    test:scalafmt

--- src/test/scala/service/SykujitsuParserSpec.scala
+++ src/test/scala/service/SykujitsuParserSpec.scala
@@ -11,8 +11,8 @@ import service.implicits.LocalDateOrderImplicits.LocalDateOrderImplicits
 import scala.collection.immutable.ListMap
 
 /**
- *
- */
+  *
+  */
 class SykujitsuParserSpec extends FunSpec with Matchers {
 
   var testOK =
@@ -113,58 +113,58 @@ class SykujitsuParserSpec extends FunSpec with Matchers {
   val resMap = SortedMap(
     2016 ->
       SortedMap(
-        LocalDate.of(2016, 1, 1) -> "元日",
-        LocalDate.of(2016, 1, 11) -> "成人の日",
-        LocalDate.of(2016, 2, 11) -> "建国記念の日",
-        LocalDate.of(2016, 3, 20) -> "春分の日",
-        LocalDate.of(2016, 4, 29) -> "昭和の日",
-        LocalDate.of(2016, 5, 3) -> "憲法記念日",
-        LocalDate.of(2016, 5, 4) -> "みどりの日",
-        LocalDate.of(2016, 5, 5) -> "こどもの日",
-        LocalDate.of(2016, 7, 18) -> "海の日",
-        LocalDate.of(2016, 8, 11) -> "山の日",
-        LocalDate.of(2016, 9, 19) -> "敬老の日",
-        LocalDate.of(2016, 9, 22) -> "秋分の日",
+        LocalDate.of(2016, 1, 1)   -> "元日",
+        LocalDate.of(2016, 1, 11)  -> "成人の日",
+        LocalDate.of(2016, 2, 11)  -> "建国記念の日",
+        LocalDate.of(2016, 3, 20)  -> "春分の日",
+        LocalDate.of(2016, 4, 29)  -> "昭和の日",
+        LocalDate.of(2016, 5, 3)   -> "憲法記念日",
+        LocalDate.of(2016, 5, 4)   -> "みどりの日",
+        LocalDate.of(2016, 5, 5)   -> "こどもの日",
+        LocalDate.of(2016, 7, 18)  -> "海の日",
+        LocalDate.of(2016, 8, 11)  -> "山の日",
+        LocalDate.of(2016, 9, 19)  -> "敬老の日",
+        LocalDate.of(2016, 9, 22)  -> "秋分の日",
         LocalDate.of(2016, 10, 10) -> "体育の日",
-        LocalDate.of(2016, 11, 3) -> "文化の日",
+        LocalDate.of(2016, 11, 3)  -> "文化の日",
         LocalDate.of(2016, 11, 23) -> "勤労感謝の日",
         LocalDate.of(2016, 12, 23) -> "天皇誕生日"
       ),
     2017 ->
       SortedMap(
-        LocalDate.of(2017, 1, 1) -> "元日",
-        LocalDate.of(2017, 1, 9) -> "成人の日",
-        LocalDate.of(2017, 2, 11) -> "建国記念の日",
-        LocalDate.of(2017, 3, 20) -> "春分の日",
-        LocalDate.of(2017, 4, 29) -> "昭和の日",
-        LocalDate.of(2017, 5, 3) -> "憲法記念日",
-        LocalDate.of(2017, 5, 4) -> "みどりの日",
-        LocalDate.of(2017, 5, 5) -> "こどもの日",
-        LocalDate.of(2017, 7, 17) -> "海の日",
-        LocalDate.of(2017, 8, 11) -> "山の日",
-        LocalDate.of(2017, 9, 18) -> "敬老の日",
-        LocalDate.of(2017, 9, 23) -> "秋分の日",
-        LocalDate.of(2017, 10, 9) -> "体育の日",
-        LocalDate.of(2017, 11, 3) -> "文化の日",
+        LocalDate.of(2017, 1, 1)   -> "元日",
+        LocalDate.of(2017, 1, 9)   -> "成人の日",
+        LocalDate.of(2017, 2, 11)  -> "建国記念の日",
+        LocalDate.of(2017, 3, 20)  -> "春分の日",
+        LocalDate.of(2017, 4, 29)  -> "昭和の日",
+        LocalDate.of(2017, 5, 3)   -> "憲法記念日",
+        LocalDate.of(2017, 5, 4)   -> "みどりの日",
+        LocalDate.of(2017, 5, 5)   -> "こどもの日",
+        LocalDate.of(2017, 7, 17)  -> "海の日",
+        LocalDate.of(2017, 8, 11)  -> "山の日",
+        LocalDate.of(2017, 9, 18)  -> "敬老の日",
+        LocalDate.of(2017, 9, 23)  -> "秋分の日",
+        LocalDate.of(2017, 10, 9)  -> "体育の日",
+        LocalDate.of(2017, 11, 3)  -> "文化の日",
         LocalDate.of(2017, 11, 23) -> "勤労感謝の日",
         LocalDate.of(2017, 12, 23) -> "天皇誕生日"
       ),
     2018 ->
       SortedMap(
-        LocalDate.of(2018, 1, 1) -> "元日",
-        LocalDate.of(2018, 1, 8) -> "成人の日",
-        LocalDate.of(2018, 2, 11) -> "建国記念の日",
-        LocalDate.of(2018, 3, 21) -> "春分の日",
-        LocalDate.of(2018, 4, 29) -> "昭和の日",
-        LocalDate.of(2018, 5, 3) -> "憲法記念日",
-        LocalDate.of(2018, 5, 4) -> "みどりの日",
-        LocalDate.of(2018, 5, 5) -> "こどもの日",
-        LocalDate.of(2018, 7, 16) -> "海の日",
-        LocalDate.of(2018, 8, 11) -> "山の日",
-        LocalDate.of(2018, 9, 17) -> "敬老の日",
-        LocalDate.of(2018, 9, 23) -> "秋分の日",
-        LocalDate.of(2018, 10, 8) -> "体育の日",
-        LocalDate.of(2018, 11, 3) -> "文化の日",
+        LocalDate.of(2018, 1, 1)   -> "元日",
+        LocalDate.of(2018, 1, 8)   -> "成人の日",
+        LocalDate.of(2018, 2, 11)  -> "建国記念の日",
+        LocalDate.of(2018, 3, 21)  -> "春分の日",
+        LocalDate.of(2018, 4, 29)  -> "昭和の日",
+        LocalDate.of(2018, 5, 3)   -> "憲法記念日",
+        LocalDate.of(2018, 5, 4)   -> "みどりの日",
+        LocalDate.of(2018, 5, 5)   -> "こどもの日",
+        LocalDate.of(2018, 7, 16)  -> "海の日",
+        LocalDate.of(2018, 8, 11)  -> "山の日",
+        LocalDate.of(2018, 9, 17)  -> "敬老の日",
+        LocalDate.of(2018, 9, 23)  -> "秋分の日",
+        LocalDate.of(2018, 10, 8)  -> "体育の日",
+        LocalDate.of(2018, 11, 3)  -> "文化の日",
         LocalDate.of(2018, 11, 23) -> "勤労感謝の日",
         LocalDate.of(2018, 12, 23) -> "天皇誕生日"
       )
@@ -176,7 +176,7 @@ class SykujitsuParserSpec extends FunSpec with Matchers {
 
       val res = resultParse match {
         case Right(result) => result
-        case Left(msg) => None
+        case Left(msg)     => None
       }
 
       res should equal(resRight)
@@ -187,7 +187,7 @@ class SykujitsuParserSpec extends FunSpec with Matchers {
 
       val res = resultParse match {
         case Right(result) => result
-        case Left(msg) => msg
+        case Left(msg)     => msg
       }
 
       res should equal("string matching regex `.*,' expected but `9' found")
@@ -200,7 +200,7 @@ class SykujitsuParserSpec extends FunSpec with Matchers {
         val resultParse = SyukujitsuParser.parse(testNG2)
         val res = resultParse match {
           case Right(result) => result
-          case Left(msg) => msg
+          case Left(msg)     => msg
         }
       }
 
@@ -210,7 +210,7 @@ class SykujitsuParserSpec extends FunSpec with Matchers {
       val resultParse = SyukujitsuParser.parse(testOK)
       val res = resultParse match {
         case Right(result) => SyukujitsuParser.convertSyukujitsuMap_for(result)
-        case Left(msg) => None
+        case Left(msg)     => None
       }
       res should equal(resMap)
     }
@@ -219,7 +219,7 @@ class SykujitsuParserSpec extends FunSpec with Matchers {
       val resultParse = SyukujitsuParser.parse(testOK)
       val res = resultParse match {
         case Right(result) => SyukujitsuParser.convertSyukujitsuMap_foldLeft(result)
-        case Left(msg) => None
+        case Left(msg)     => None
       }
       res should equal(resMap)
     }
@@ -228,7 +228,7 @@ class SykujitsuParserSpec extends FunSpec with Matchers {
       val resultParse = SyukujitsuParser.parse(testOK)
       val res = resultParse match {
         case Right(result) => SyukujitsuParser.convertSyukujitsuMap_recursive(result)
-        case Left(msg) => None
+        case Left(msg)     => None
       }
       res should equal(resMap)
     }
@@ -237,7 +237,7 @@ class SykujitsuParserSpec extends FunSpec with Matchers {
       val resultParse = SyukujitsuParser.parse(testOK)
       val res: Option[Map[Int, Map[LocalDate, String]]] = resultParse match {
         case Right(result) => Some(SyukujitsuParser.convertSyukujitsuMap_builtin(result))
-        case Left(msg) => None
+        case Left(msg)     => None
       }
 
       // println(ListMap(res.get.toSeq.sortBy(_._1):_*))
